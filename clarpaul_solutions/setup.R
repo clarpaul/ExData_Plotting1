1761298e8dd8499fbf2e1611838e7b1e5b9ef2a3
clarpaul <clarpaul@gmail.com>
2016-11-14 07:27:46 -0800

    Version of 'setup.R' with more verbose output

--- clarpaul_solutions/setup.R
+++ clarpaul_solutions/setup.R
@@ -3,24 +3,29 @@
 ## Download zipfile into working directory, read data, reformat Date & Time info as POSIXct datetime
 
 if (!file.exists("household_power_consumption.zip")) {
+    cat("Downloading file 'household_power_consumption.zip'.\n")
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
                   "household_power_consumption.zip")
 } else {
-    cat("File 'household_power_consumption.zip' already exists.\nChecking if data is loaded...\n")
+    cat("File 'household_power_consumption.zip' has already been downloaded.\nChecking if data is loaded...\n")
 }
 
 if (!("powerdf" %in% ls()) || nrow(powerdf) != 2880) { # nrow(powerdf) evaulated iff "powerdf" in ls()
+    
+    cat("Reading compressed data...")
     classes <- c("character", "character", rep("numeric", times = 7))
     conxn <- unz("household_power_consumption.zip","household_power_consumption.txt")
-    cat("Reading compressed data...")
     powerdf <- read.csv(conxn, sep = ";", na.strings = "?", colClasses = classes) # closes conxn after use
+    
     cat("Preparing data...")
     powerdf <- powerdf[powerdf$Date == "1/2/2007" | powerdf$Date == "2/2/2007",]
     datetime <- paste(powerdf$Date, powerdf$Time)
     datetime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
     powerdf <- cbind(datetime,powerdf)
     rm(list = c("classes","conxn","datetime"))
+    
     cat("Data ready for use.")
+    
 } else {
     cat("Data is loaded and ready for use.")
 }
